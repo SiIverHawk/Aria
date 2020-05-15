9fdb7dcc3c3c0b7ea3020ad904cab57d00178de5
SiIverHawk <miguelnegro100@gmail.com>
2020-05-15 15:59:55 -0600

    added fontAwesome devDependencies and minified css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 	<!-- Styles -->
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
 </head>
 
